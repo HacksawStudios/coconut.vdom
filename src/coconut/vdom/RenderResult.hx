06b4945d3ec08bfcde15390984616e317b2a0ff5
Tomasz Kwiatek <tomasz@hacksawstudios.com>
2023-06-19 16:07:33 +0200

    add: Add dynamic types support

--- src/coconut/vdom/RenderResult.hx
+++ src/coconut/vdom/RenderResult.hx
@@ -5,23 +5,27 @@ import js.html.*;
 
 @:pure
 abstract RenderResult(VNode<Node>) to VNode<Node> from VNode<Node> {
+	inline function new(n)
+		this = n;
 
-  inline function new(n) this = n;
+	@:from static inline function ofText(s:String):RenderResult
+		return if (s == null) null; else coconut.vdom.Html.text(s);
 
-  @:from static inline function ofText(s:String):RenderResult
-    return
-      if (s == null) null;
-      else coconut.vdom.Html.text(s);
+	@:from static function ofInt(i:Int):RenderResult
+		return ofText('$i');
 
-  @:from static function ofInt(i:Int):RenderResult
-    return ofText('$i');
+	@:from static function ofFloat(f:Float):RenderResult
+		return ofText('$f');
 
-  @:from static function ofNode(n:Node):RenderResult
-    return VNode.embed(n);
+	@:from static function ofDynamic(d:Dynamic):RenderResult
+		return ofText('$d');
 
-  static public function fragment(attr:{}, children:Children):RenderResult
-    return VNode.many(children);
+	@:from static function ofNode(n:Node):RenderResult
+		return VNode.embed(n);
 
-  // @:from static function ofView(v:coconut.vdom.View):RenderResult
-  //   return VWidgetInst(v);
+	static public function fragment(attr:{}, children:Children):RenderResult
+		return VNode.many(children);
+
+	// @:from static function ofView(v:coconut.vdom.View):RenderResult
+	//   return VWidgetInst(v);
 }
